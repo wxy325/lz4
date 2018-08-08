3365a75eb5f6f9df7865c2223b08d42a717573fb
wxy325 <wxy325@qq.com>
2018-08-08 02:46:49 -0700

    adjust comment

--- lib/utf8Util.c
+++ lib/utf8Util.c
@@ -3,22 +3,20 @@
 
 
 size_t utf8Length(char initChar) {
-    // Invalid 0 byte
-    // 0zzzzzzz  1 byte
-    // 110yyyyy  2 byte2
-    // 1110xxxx  3 bytes
-    // 11110www  4 bytes
-
-
     if ((initChar & 0x80) == 0) {
+        // 0zzzzzzz  1 byte
         return 1;
     } else if ((initChar & 0xe0) == 0xc0) {
+        // 110yyyyy  2 bytes
         return 2;
     } else if ((initChar & 0xf0) == 0xe0) {
+        // 1110xxxx  3 bytes
         return 3;
     } else if ((initChar & 0xf8) == 0xf0) {
+        // 11110www  4 bytes
         return 4;
     } else {
+        // Invalid  return 0
         return 0;
     }
 }
